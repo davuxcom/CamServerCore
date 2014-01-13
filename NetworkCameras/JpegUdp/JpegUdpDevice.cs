f8ddc6a77c50ea33a4ec1307fdb68c1a6c6045e5
Dave Amenta <dave@daveamenta.com>
2014-01-12 21:04:06 -0800

    reliability fixes

--- NetworkCameras/JpegUdp/JpegUdpDevice.cs
+++ NetworkCameras/JpegUdp/JpegUdpDevice.cs
@@ -1,86 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using CamServerCore;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using AForge.Video;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows;
-using System.Net;
-using System.Diagnostics;
-using CamServerCore;
-using DavuxLib2.Platform;
-using System.Threading;
-using System.Net.Sockets;
-using System.IO;
 
 namespace NetworkCameras.JpegUdp
 {
     class JpegUdpDevice : BaseVideoDevice, IDisposable
     {
         int Port = 0;
+        Thread worker = null;
+        UdpClient udpClient = null;
         public JpegUdpDevice(string DisplayName, string Port)
         {
             this.Port = int.Parse(Port);
-            UID = "MJpeg-" + Port;
-            Name = string.Format("{0} ({1})", DisplayName, Port);
+            UID = "MJpeg-Udp-" + Port;
+            Name = string.Format("{0} (Port {1})", DisplayName, Port);
         }
-
-        Thread worker = null;
-
         protected override void StartVideoInternal()
         {
-            worker = new Thread(() =>
-            {
+            worker = new Thread(() => {
                 try
                 {
-                    var u = new UdpClient(new IPEndPoint(IPAddress.Any, Port));
-
+                    udpClient = new UdpClient(new IPEndPoint(IPAddress.Any, Port));
                     while (worker == Thread.CurrentThread)
                     {
                         IPEndPoint ip = null;
-                        var bytes = u.Receive(ref ip);
-                        Console.WriteLine(bytes.Length);
-
-                        JpegBitmapDecoder decoder = null;
-                        BitmapSource bitmapSource = null;
+                        var bytes = udpClient.Receive(ref ip);
+                        // Console.WriteLine(bytes.Length);
                         using (var stream = new MemoryStream(bytes))
                         {
-                            decoder = new JpegBitmapDecoder(stream,
+                            var decoder = new JpegBitmapDecoder(stream,
                                                             BitmapCreateOptions.PreservePixelFormat,
                                                             BitmapCacheOption.OnLoad);
+                            var bitmapSource = decoder.Frames[0];
+                            bitmapSource.Freeze();
+
+                            var rotatedSource = new TransformedBitmap();
+                            rotatedSource.BeginInit();
+                            rotatedSource.Source = bitmapSource;
+                            rotatedSource.Transform = new RotateTransform(90);
+                            rotatedSource.EndInit();
+
+                            this.FrameArrived(rotatedSource);
                         }
-                        bitmapSource = decoder.Frames[0];
-                        bitmapSource.Freeze();
-
-
-                        TransformedBitmap myRotatedBitmapSource = new TransformedBitmap();
-                        myRotatedBitmapSource.BeginInit();
-                        myRotatedBitmapSource.Source = bitmapSource;
-                        myRotatedBitmapSource.Transform = new RotateTransform(90);
-                        myRotatedBitmapSource.EndInit();
-
-                        this.FrameArrived(myRotatedBitmapSource);
                     }
                 }
                 catch (Exception ex)
                 {
                     Trace.WriteLine("Worker crash: " + ex.Message);
+                    StopVideoInternal();
                     StartVideoInternal();
+                    return;
                 }
+
+                StopVideoInternal();
             });
             worker.Start();
         }
-
         protected override void StopVideoInternal()
         {
             worker = null;
+            if (udpClient != null)
+            {
+                try
+                {
+                    udpClient.Close();
+                }
+                catch (SocketException) { }
+            }
         }
-
         public void Dispose()
         {
         }
